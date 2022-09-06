5faec985661acf0ab55bc719b32975c15dc92c8c
suriyaprabha-R <suriyaprabhatest1989@gmail.com>
2022-09-06 22:50:47 +0530

    suriyaprabha15011989's code

--- src/test/java/org/test/Sample.java
+++ src/test/java/org/test/Sample.java
@@ -2,7 +2,12 @@ package org.test;
 
 public class Sample {
 	public static void main(String[] args) {
+		System.out.println("Test1");
+		System.out.println("Test4");
+		System.out.println("Test5");
 		
+		System.out.println("Test 6");
+		System.out.println("Test 7");
 	}
 
 }
