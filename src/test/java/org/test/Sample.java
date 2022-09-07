9430f63e1f3aa010429149dee31ad334b906367c
suriyaprabha-R <suriyaprabhatest1989@gmail.com>
2022-09-07 22:56:05 +0530

    Resloved Conflicts

--- src/test/java/org/test/Sample.java
+++ src/test/java/org/test/Sample.java
@@ -7,8 +7,6 @@ public class Sample {
 		System.out.println("Test3");
 		System.out.println("Test4");
 		System.out.println("Test5");
-		System.out.println("Test2");
-		System.out.println("Test3");
 		// >>>>>>> ab3f51bf493819efd26b3bb575b8c58e187f4fd9-It is called
 		// session id
 
